bac2483da10041ef92e6b92a02a5b247d5939f77
Joel Binnquist <joel.binnquist@gmail.com>
2026-04-16 18:15:07 +0200

    remove counsel editor

--- src/app/features/counsel-editor/counsel-detail-editors/counsel-detail-editor.ts
+++
@@ -1,6 +0,0 @@
-import { signal } from '@angular/core'
-import { CounselDetailId } from '../model/counsel.model'
-
-export class CounselDetailEditor {
-  readonly counselDetailId = signal<CounselDetailId | null>(null)
-}
