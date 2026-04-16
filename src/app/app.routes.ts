500863a7c97d2c5c9488166203b918e07a4c7e63
Joel Binnquist <joel.binnquist@gmail.com>
2026-04-16 18:27:31 +0200

    Bump to Angular 21

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,10 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
 import { StepperPage } from './features/stepper/stepper-page.component'
-import { CounselEditorPage } from './features/counsel-editor/counsel-editor-page.component'
 
 export const routes: Routes = [
   {
     title: "Stepper",
     component: StepperPage,
     path: "stepper"
-  },
-  {
-    title: "Counsel editor",
-    component: CounselEditorPage,
-    path: "counsel-editor"
   }
 ];
